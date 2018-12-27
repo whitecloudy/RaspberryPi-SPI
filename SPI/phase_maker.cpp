f51e74b50486fe4a7d8a1a88d22022daae499d0b
whitecloudy <alex9395@naver.com>
2018-12-27 23:03:36 +0900

    phase_maker update

--- SPI/phase_maker.cpp
+++ SPI/phase_maker.cpp
@@ -5,23 +5,28 @@
 #include "Vout_controller.h"
 #include "phase_attenuator_controller.h"
 
-#define sleep_microtime 500
-#define DEBUG_MODE true
+#define DEBUG_MODE false
 
 int main(int argc, char *argv[]){
   Vout_controller v_con;
 
-  if(argc != 2){
+  if(argc != 3){
     std::cout<<"notenough argument"<<std::endl;
     return 0;
   }
 
   int antenna_num = std::atoi(argv[1]);
+  int sleep_microtime = std::atoi(argv[2]);
+
   int attenuator_num = ATTENUATOR[antenna_num];
   int phase_num = PHASE[antenna_num];
+  int target_num_p, target_num_a;
+
+  target_num_p = phase_num;
+  target_num_a = attenuator_num;
+
 
   v_con.voltage_modify(ALL_vout_num, 0);
-  v_con.voltage_modify(attenuator_num, 1);
   v_con.data_apply();
 
   float voltage = 0.1;
@@ -30,48 +35,63 @@ int main(int argc, char *argv[]){
 
 
   if(!DEBUG_MODE){
-    v_con.voltage_modify(attenuator_num, 0);
+    v_con.voltage_modify(target_num_a, 1);
     v_con.data_apply();
     delayMicroseconds(sleep_microtime);
 
-    v_con.voltage_modify(attenuator_num, 1);
+    v_con.voltage_modify(target_num_a, 0);
     v_con.data_apply();
     delayMicroseconds(sleep_microtime);
 
-    v_con.voltage_modify(attenuator_num, 0);
+    v_con.voltage_modify(target_num_a, 1);
+    v_con.data_apply();
+    delayMicroseconds(sleep_microtime);
+
+    v_con.voltage_modify(target_num_a, 0);
     v_con.data_apply();
     delayMicroseconds(sleep_microtime);
 
     for(; voltage <= 10; voltage += 0.1){
-      v_con.voltage_modify(phase_num, voltage);
+      v_con.voltage_modify(target_num_p, voltage);
       v_con.data_apply();
       delayMicroseconds(sleep_microtime);
     }
-    v_con.voltage_modify(phase_num, 0);
+
+    v_con.voltage_modify(ALL_vout_num, 0);
+    v_con.data_apply();
+    delayMicroseconds(sleep_microtime);
+
+   // for(voltage = 0.1; voltage <= 10; voltage += 0.1){
+   //   v_con.voltage_modify(target_num_a, voltage);
+   //   v_con.data_apply();
+   //   delayMicroseconds(sleep_microtime);
+   // }
+
+    v_con.voltage_modify(ALL_vout_num, 0);
     v_con.data_apply();
     delayMicroseconds(sleep_microtime);
   }else{
-    v_con.voltage_modify(phase_num, 5);
+    v_con.voltage_modify(target_num_a, 5);
     v_con.data_apply();
     delayMicroseconds(250);
 
-    v_con.voltage_modify(phase_num, 0);
+    v_con.voltage_modify(target_num_a, 0);
     v_con.data_apply();
     delayMicroseconds(250);
 
-    v_con.voltage_modify(phase_num, 5);
+    v_con.voltage_modify(target_num_a, 5);
     v_con.data_apply();
     delayMicroseconds(500);
 
-    v_con.voltage_modify(phase_num, 0);
+    v_con.voltage_modify(target_num_a, 0);
     v_con.data_apply();
     delayMicroseconds(500);
 
-    v_con.voltage_modify(phase_num, 5);
+    v_con.voltage_modify(target_num_a, 5);
     v_con.data_apply();
     delayMicroseconds(1000);
 
-    v_con.voltage_modify(phase_num, 0);
+    v_con.voltage_modify(target_num_a, 0);
     v_con.data_apply();
     delayMicroseconds(1000);
 
@@ -79,7 +99,7 @@ int main(int argc, char *argv[]){
 
   }
 
-
-
+  std::cout <<"done"<<std::endl;
+  std::cin >> dummy;
   return 0;
 }
