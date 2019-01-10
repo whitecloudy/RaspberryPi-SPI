a7cc8af991a085c0d5406ddca22b8bc43a1d8d82
Whitecloud <alex9395@naver.com>
2019-01-10 20:31:59 +0900

    Phase attenuator controller update

---
+++ SPI/Phase_Attenuator_controller.cpp
@@ -0,0 +1,96 @@
+#include "Phase_Attenuator_controller.h"
+#include <string>
+#include <cmath>
+
+int Phase_Attenuator_controller::load_cal_data(void){
+  //loading the calibration data
+  for(int i = 0; i<16; i++){
+    std::string filename = "calibration_data/ant" + std::to_string(i) + "_cal_data";
+    io::CSVReader<4> csv_reader(filename);
+
+    float ph_V, po_V, phase, power;
+    int j = 0;
+    while(csv_reader.read_row(phase, power, ph_V, po_V)){
+      V_preset[i][j].phase = phase;
+      V_preset[i][j].power = power;
+      V_preset[i][j].ph_V = ph_V;
+      V_preset[i][j].po_V = po_V;
+    }
+  }
+
+  return 0;
+ 
+ 
+}
+
+inline int preset_finder(struct cal_ref * V_preset, int start, int end, float phase){
+  int middle = (start + end)/2;
+
+  if((V_preset[middle].phase < phase) && (V_preset[middle+1].phase >= phase)){
+    if((phase - V_preset[middle].phase) > (V_preset[middle+1].phase - phase))
+      return middle+1;
+    else
+      return middle;
+  }
+
+  if(V_preset[middle].phase <= phase)
+    return preset_finder(V_preset, start, middle, phase);
+  else
+    return preset_finder(V_preset, middle+1, end, phase);
+  
+}
+
+int Phase_Attenuator_controller::find_matched_preset(int ant, float phase){
+  return preset_finder(V_preset[ant], 0, CAL_data_length-1, phase);
+}
+
+int Phase_Attenuator_controller::set_integer_index(void){
+  for(int ant = 0; ant < ANT_num; ant++){
+    for(int i = 0; i < 360; i++){
+      index_V_preset[ant][i] = find_matched_preset(ant, i);
+    }
+  }
+}
+
+int Phase_Attenuator_controller::voltage_index_search(int ant, int phase){
+  return index_V_preset[ant][phase%360];
+}
+
+int Phase_Attenuator_controller::voltage_index_search(int ant, float phase){
+  while(phase > 360)
+    phase -= 360.0;
+  while(phase < 0)
+    phase += 360.0;
+
+  return find_matched_preset(ant, phase);
+}
+
+int Phase_Attenuator_controller::phase_setup(int ant, int index){
+  int at_result = V.voltage_modify(ATTENUATOR[ant], V_preset[ant][index].po_V);
+  int ph_result = V.voltage_modify(PHASE[ant], V_preset[ant][index].ph_V);
+
+  return at_result&&ph_result;
+}
+
+int Phase_Attenuator_controller::phase_control(int ant, int phase){
+  int index = voltage_index_search(ant, phase);
+
+  return phase_setup(ant, index);
+}
+
+int Phase_Attenuator_controller::phase_control(int ant, float phase){
+  int index = voltage_index_search(ant, phase);
+
+  return phase_setup(ant, index);
+}
+
+int Phase_Attenuator_controller::data_apply(void){
+  return V.data_apply();
+}
+
+
+Phase_Attenuator_controller::Phase_Attenuator_controller(void){
+  if(!load_cal_data())
+    std::cout<<"Error : Loading calibration data failed"<<std::endl;
+  set_integer_index();
+}
--- SPI/Phase_Attenuator_controller.h
+++ SPI/Phase_Attenuator_controller.h
@@ -3,6 +3,9 @@
 
 #include <iostream>
 #include "Vout_controller.h"
+#include "csv/csv.h"
+
+#define ANT_num 16
 
 #define ANT1_phase 23
 #define ANT2_phase 22
@@ -38,15 +41,40 @@
 #define ANT15_attenuator 33
 #define ANT16_attenuator 32
 
+#define CAL_data_length 199
+
 const char PHASE[] = {ANT1_phase, ANT2_phase, ANT3_phase, ANT4_phase, ANT5_phase, ANT6_phase, ANT7_phase, ANT8_phase, ANT9_phase, ANT10_phase, ANT11_phase, ANT12_phase, ANT13_phase, ANT14_phase, ANT15_phase, ANT16_phase};
 const char ATTENUATOR[] = {ANT1_attenuator, ANT2_attenuator, ANT3_attenuator, ANT4_attenuator, ANT5_attenuator, ANT6_attenuator, ANT7_attenuator, ANT8_attenuator, ANT9_attenuator, ANT10_attenuator, ANT11_attenuator, ANT12_attenuator, ANT13_attenuator, ANT14_attenuator, ANT15_attenuator, ANT16_attenuator};
 
-class Phase_attenuator_controller{
+struct cal_ref{
+  float phase;
+  float power;
+  float ph_V;
+  float po_V;
+};
+
+
+class Phase_Attenuator_controller{
   private:
+    //Voltage control reference data
+    struct cal_ref V_preset[ANT_num][CAL_data_length];
+    int index_V_preset[ANT_num][360];
+
     Vout_controller V;
+    int load_cal_data(void);
+    int find_matched_preset(int, float);
+    int set_integer_index(void);
+
+    int voltage_index_search(int, int);
+    int voltage_index_search(int, float);
+
+    int phase_setup(int, int);
 
   public:
-    
+    Phase_Attenuator_controller(void);
+    int phase_control(int, int);
+    int phase_control(int, float);
+    int data_apply();
 };
 
 #endif
