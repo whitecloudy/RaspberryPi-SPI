67d3bb9c413d87655b5a857372299a3388cb8860
whitecloudy <alex9395@naver.com>
2019-01-12 00:10:08 +0900

    phase_control update

--- SPI/phase_control.cpp
+++ SPI/phase_control.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
+#include <cstdlib>
 #include "Phase_Attenuator_controller.h"
 
-int main(void){
+int main(int argc, char **argv){
   int ant = 0;
   float phase = 0;
   Phase_Attenuator_controller ctrl(0);
-  
-   while(true){
-    std::cout << "Ant : "<<std::endl;
-    std::cin >> ant;
-    if(ant = -1)
-      break;
-    std::cout << "Phase : "<<std::endl;
-    std::cin >> phase;
+ 
+  int dummy = 0;
+  ant = atoi(argv[1]);
 
-    ctrl.phase_control(ant, phase);    
+  for(int i = 0; i<360; i+=30){
+    std::cout<<"phase = "<<i<<std::endl;
+
+    ctrl.phase_control(ant,i);
     ctrl.data_apply();
+
+    std::cin>>dummy;
   }
+  
 
   return 0;
 }
